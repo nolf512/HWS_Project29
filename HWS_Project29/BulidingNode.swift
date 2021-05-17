33de11aa025892c81a14fbbca1d5fa7cb59f4a23
nofprog <k.jin3217@gmail.com>
2021-05-17 19:28:20 +0900

    Initial Commit

---
+++ HWS_Project29/BulidingNode.swift
@@ -0,0 +1,27 @@
+//
+//  BulidingNode.swift
+//  HWS_Project29
+//
+//  Created by J on 2021/05/17.
+//
+
+import SpriteKit
+import UIKit
+
+class BulidingNode: SKSpriteNode {
+
+    var currentImage: UIImage!
+    
+    func setup(){
+        
+    }
+    
+    
+    
+    func configurePhysics(){
+        
+    }
+    
+    
+    
+}
---
+++ HWS_Project29/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  HWS_Project29
+//
+//  Created by J on 2021/05/17.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    enum CollitionType: UInt32 {
+        case banana = 1
+        case building = 2
+        case player = 4
+    }
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
